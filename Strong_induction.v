5602c3226211237174f48f4fd0ad78b4524e5cca
Caitlin D'Abrera <35909579+caitlindabrera@users.noreply.github.com>
2018-11-05 11:22:40 +1100

    Initial commit.

---
+++ Strong_induction.v
@@ -0,0 +1,12 @@
+Require Import Arith Init.Wf.
+Definition strong_induction_wf := well_founded_induction lt_wf.
+
+Theorem strong_induction :  forall P : nat -> Prop,
+    P 0 -> (forall n : nat, (forall m, m <= n -> P m) -> P (S n)) ->
+    forall n : nat, P n.
+Proof.
+  intros P H0 H1 n. induction n using strong_induction_wf.
+  destruct n. assumption.
+  apply H1.  intros m Hm. apply H. unfold lt.
+  apply le_n_S.  assumption.
+Qed.
