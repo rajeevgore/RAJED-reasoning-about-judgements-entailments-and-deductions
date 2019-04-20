6cf35757b230b5e115ada97dd6c9ef01d9eafbdc
Jeremy Dawson <jeremy@cecs.anu.edu.au>
2019-04-20 12:28:32 +1000

    proof of exchange on left for propositional rules, using generic version of step lemma

--- lntls.v
+++ lntls.v
@@ -18,125 +18,8 @@ Require Import List_lemmas.
 Require Import lnt.
 Require Import lntacs.
 
-(* proof using step lemma *)
-Lemma gen_swapL_step_pr: forall V ps concl 
-  (last_rule rules : rls (list (rel (list (PropF V)) * dir))),
-  last_rule = nsrule (seqrule (@princrule V)) ->
-  gen_swapL_step last_rule rules ps concl.
-Proof.  intros until 0.  unfold gen_swapL_step.
-intros lreq nsr drs acm rs. subst.
-
-inversion nsr as [? ? ? K ? sppc mnsp nsc].
-unfold nsext in nsc.
-unfold can_gen_swapL.   intros until 0. intros pp ss.
-unfold nsext in pp.
-
-apply partition_2_2 in pp.
-remember (Γ1 ++ Γ3 ++ Γ2 ++ Γ4, Δ) as seqe.
-
-decompose [or] pp. 
-
-{ nsright pp G0 seqe d0 x Ge HeqGe
-  K d ps ps0 inps0 pse K0 drs nsr acm G seq rs. }
-all : revgoals.
-{ nsleft pp G0 seqe d0 x He HeqHe
-  K d ps ps0 inps0 pse K0 drs nsr acm G seq rs. }
-
-(* now case where move and rule application occur in the same sequent *)
-cE. clear pp. injection H0 as.
-inversion sppc as [? ? ? ? ? ? pr mse se].
-unfold seqext in se.
-subst.  clear nsr. clear sppc.
-destruct c0. 
-injection se as sel ser.
-subst.
-(* do as much as possible for all rules at once *)
-acacD' ; (* gives 10 subgoals *)
-subst ;
-repeat ((list_eq_nc || (pose pr as Qpr ; apply princrule_L_oe in Qpr)) ;
-  sD ; subst ; simpl ; simpl in pr ;
-  try (rewrite app_nil_r) ; try (rewrite app_nil_r in pr)).
-
-(* need to do stage1 rs pr. to see what next,
-  and stage12ds rs acm qin1 qin3 pr ...,
-  then all : solve_eqs. to see what next *)
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-rewrite (app_assoc l) ; reflexivity.  }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-rewrite (app_assoc sel) ; reflexivity.  }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-  reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ; 
-rewrite (app_assoc l1) ; rewrite (app_assoc sel) ; reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-  reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-rewrite (app_assoc sel1) ; reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-rewrite (app_assoc l1) ; rewrite (app_assoc sel1) ; reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-  reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-rewrite (app_assoc l) ; reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-rewrite (app_assoc Φ1) ; reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-  reflexivity. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs. }
-
-{ stage12altdsLg (@princrule V) rs drs acm qin1 qin3 pr ; solve_eqs ;
-rewrite (app_assoc l1) ; rewrite (app_assoc Φ1) ; reflexivity. }
-
-Qed.
-
-Check gen_swapL_step_pr.
-
-Lemma gen_swapL: forall {V : Set} ns
-  (D : derrec (nsrule (seqrule (@princrule V))) (fun _ => False) ns),
-  can_gen_swapL (nsrule (seqrule (@princrule V))) ns.
-
-Proof. 
-intro.  intro.  intro.
-eapply derrec_all_ind in D.
-exact D. tauto.
-intros. inversion H. 
-subst.
-pose gen_swapL_step_pr.
-unfold gen_swapL_step in g.
-eapply g. reflexivity. eassumption. assumption. assumption.
-unfold rsub. clear g. 
-intros.  assumption.
-Qed.
-
-Check gen_swapL.
-
-(* version generic as regards rules satisfying rules_L_oe *)
+(* proof of exchange using step lemma *)
+(* version of step lemma generic as regards rules satisfying rules_L_oe *)
 Lemma gen_swapL_step_loe: forall V ps concl princrules
   (last_rule rules : rls (list (rel (list (PropF V)) * dir))),
   rules_L_oe princrules -> 
@@ -245,3 +128,31 @@ Lemma gen_swapL_step_Id: forall V ps concl
 Proof.  intros. eapply gen_swapL_step_loe. apply Idrule_L_oe'. exact H. Qed.
 
 Check gen_swapL_step_Id.
+
+Lemma gen_swapL_step_pr: forall V ps concl 
+  (last_rule rules : rls (list (rel (list (PropF V)) * dir))),
+  last_rule = nsrule (seqrule (@princrule V)) ->
+  gen_swapL_step last_rule rules ps concl.
+Proof.  intros. eapply gen_swapL_step_loe. apply princrule_L_oe'. exact H. Qed.
+
+Check gen_swapL_step_pr.
+
+Lemma gen_swapL: forall {V : Set} ns
+  (D : derrec (nsrule (seqrule (@princrule V))) (fun _ => False) ns),
+  can_gen_swapL (nsrule (seqrule (@princrule V))) ns.
+
+Proof. 
+intro.  intro.  intro.
+eapply derrec_all_ind in D.
+exact D. tauto.
+intros. inversion H. 
+subst.
+pose gen_swapL_step_pr.
+unfold gen_swapL_step in g.
+eapply g. reflexivity. eassumption. assumption. assumption.
+unfold rsub. clear g. 
+intros.  assumption.
+Qed.
+
+Check gen_swapL.
+
