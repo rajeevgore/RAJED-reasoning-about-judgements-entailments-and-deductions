3947d0f0fc84bf0b41e7ffe9d406fa2f1ea83772
Jeremy Dawson <jeremy@cecs.anu.edu.au>
2020-11-30 11:02:55 +1100

    removed swapped from defn of ms_ord, not needed

--- ljt/ljt_dnterm.v
+++ ljt/ljt_dnterm.v
@@ -18,11 +18,16 @@ Require Import Coq.Program.Basics.
 
 (* multiset ordering, but on lists - allowing swap (hopefully 1 swap not rtc *) 
 Inductive ms_ord {U} R : relationT (list U) :=
-    ms_ordI : forall G1 G2 ps c Gsw, (forall p : U, InT p ps -> R p c) ->
-      swapped (G1 ++ ps ++ G2) Gsw -> ms_ord R Gsw (G1 ++ c :: G2).
+    ms_ordI : forall G1 G2 ps c, (forall p : U, InT p ps -> R p c) ->
+    ms_ord R (G1 ++ ps ++ G2) (G1 ++ c :: G2).
 
-Lemma ms_ordI_eqc {U} R G1 G2 ps c Gsw G1c2 :
-  (forall p : U, InT p ps -> R p c) -> swapped (G1 ++ ps ++ G2) Gsw ->
+Lemma ms_ordI_eqc {U} R G1 G2 ps c G1c2 :
+  (forall p : U, InT p ps -> R p c) -> 
+    G1c2 = (G1 ++ c :: G2) -> ms_ord R (G1 ++ ps ++ G2) G1c2.
+Proof. intros. subst. eapply ms_ordI ; eassumption. Qed.
+
+Lemma ms_ordI_eq {U} R G1 G2 ps c Gsw G1c2 :
+  (forall p : U, InT p ps -> R p c) -> Gsw = G1 ++ ps ++ G2 ->
     G1c2 = (G1 ++ c :: G2) -> ms_ord R Gsw G1c2.
 Proof. intros. subst. eapply ms_ordI ; eassumption. Qed.
 
@@ -66,11 +71,11 @@ destruct H1 ; destruct i ; inversion inp ; subst ; clear inp ;
 unfold flip in H0 ; inversion H0 ; clear H0 ; subst.
 + eapply (@ms_ordI _ _ [pr] [] [_]).
 intros p inp. inversion inp. subst. apply dnsub_Imp_AndL.
-inversion H0. simpl. apply swapped_same.
+inversion H0. 
 + eapply (@ms_ordI _ _ [pr] [] [_; _]).
 intros p inp. inversion inp ; subst ; clear inp. apply dnsub_Imp_OrL1.
 inversion H0 ; clear H0 ; subst. apply dnsub_Imp_OrL2.
-inversion H1.  simpl. apply swapped_same.  Qed.
+inversion H1.  Qed.
 
 Lemma lrls_prems_dn V G ps cl cr pl pr :
   rlsmap (flip pair G) (@LJslrules V) ps (cl, cr) ->
@@ -83,23 +88,23 @@ unfold flip in H0 ; inversion H0 ; clear H0 ; subst.
 eapply (@ms_ordI _ _ [pr] [] [_; _]).
 intros p inp. inversion inp. subst. clear inp. apply dnsub_AndL.
 inversion H0 ; clear H0 ; subst. apply dnsub_AndR.
-inversion H3. simpl. apply swapped_same.
+inversion H3. 
 - destruct o ; inversion inp ; subst ; clear inp ;
 unfold flip in H0 ; inversion H0 ; clear H0 ; subst.
 eapply (@ms_ordI _ _ [pr] [] [_]).
 intros p inp. inversion inp. subst. apply dnsub_OrL.
-inversion H0. simpl. apply swapped_same.
+inversion H0. 
 inversion H1. clear H1. subst.
 eapply (@ms_ordI _ _ [pr] [] [_]).
 intros p inp. inversion inp. subst. apply dnsub_OrR.
-inversion H0. simpl. apply swapped_same.
+inversion H0. 
 inversion H1.
 - destruct b. inversion inp. Qed.
 
 Lemma prem_dn_lem U R (x y : U) : R x y -> ms_ord R [x] [y].
 Proof. intro rxy.  eapply (@ms_ordI _ _ [] [] [_]).
 intros p inp. inversion inp. subst. apply rxy.
-inversion X. simpl. apply swapped_same. Qed.
+inversion X. Qed.
 
 Lemma rrls_prems_dn V ps cl cr pl pr :
   rlsmap (pair []) (@LJsrrules V) ps (cl, cr) ->
@@ -137,7 +142,7 @@ inversion H0 ; subst ; clear H0.
 eapply (@ms_ordI _ _ [] [] [_; _]).
 intros p inp. inversion inp. subst. clear inp. apply dnsub_ImpR.
 inversion H0 ; subst ; clear H0. apply dnsub_ImpL.
-inversion H3. simpl. apply swapped_same.
+inversion H3. 
 inversion H0. Qed.
 
 Lemma Imp_prems_dn V ps cl cr pl pr : 
@@ -148,8 +153,8 @@ inversion ljpc ; subst ; clear ljpc.
 inversion inp ; subst ; clear inp.
 - inversion H0 ; subst ; clear H0.
 apply (@tT_trans _ _ _ [Imp (Imp C pr) B]) ; apply tT_step.
-+ eapply (ms_ordI_eqc _ [] []). 3: reflexivity.
-2: rewrite app_nil_r. 2: apply swapped_same.
++ eapply (ms_ordI_eq _ [] []). 3: reflexivity.
+2: rewrite app_nil_r. 2: reflexivity.
 intros p inp.  inversion inp ; subst ; clear inp.
 eapply tT_trans ; apply tT_step.  apply dnsub_ImpR.  apply dnsub_ImpL.
 inversion H0 ; subst ; clear H0.
@@ -157,13 +162,13 @@ apply tT_step. apply dnsub_Imp_ImpL.
 inversion H1 ; subst ; clear H1.
 eapply tT_trans ; apply tT_step.  apply dnsub_ImpL.  apply dnsub_ImpL.
 inversion H0.
-+ eapply (ms_ordI_eqc _ [] [_]).
-intros p inp. apply (InT_nilE _ inp).  apply swapped_same. reflexivity.
++ eapply (ms_ordI_eq _ [] [_]).
+intros p inp. apply (InT_nilE _ inp).   reflexivity. reflexivity.
 
 - inversion H0 ; subst ; clear H0.
 inversion H1 ; subst ; clear H1.
-apply tT_step. eapply (ms_ordI_eqc _ [_] []).
-3: reflexivity.  2: rewrite app_nil_r. 2: apply swapped_same.
+apply tT_step. eapply (ms_ordI_eq _ [_] []).
+3: reflexivity.  2: rewrite app_nil_r. 2: reflexivity.
 intros p inp.  inversion inp ; subst ; clear inp.
 apply tT_step.  apply dnsub_ImpR.
 inversion H0.  inversion H1.  Qed.
@@ -173,7 +178,7 @@ Lemma Imp_p_rprem_dn V lp cl cr pl pr :
   @ImpLrule_p V [lp ; (pl, pr)] (cl, cr) -> 
   ms_ord dnsubfml (pr :: pl) (cr :: cl).
 Proof. intro r. inversion r. subst. clear r.
-eapply (@ms_ordI _ _ [cr] [] [B]).  2: apply swapped_same.
+eapply (@ms_ordI _ _ [cr] [] [B]). 
 intros * inp.  inversion inp ; subst ; clear inp. apply dnsub_ImpR.
 inversion H0. Qed.
 
@@ -186,7 +191,7 @@ Lemma Imp_p_lprem_dn V rp cl cr pl pr :
 Proof. intro r. inversion r. subst. clear r.
 destruct cr.
 left. simpl. reflexivity.
-all: right ; eapply (@ms_ordI _ _ [] [_] [_]) ; try (apply swapped_same) ;
+all: right ; eapply (@ms_ordI _ _ [] [_] [_]) ; 
 intros p0 inp ; inversion inp ; try (inversion H0) ; subst ; clear inp ;
 unfold measure ; simpl ; apply Nat.lt_0_succ.
 Qed.
