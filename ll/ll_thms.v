4148a212e87282d01576c37d34a24eb31b7c41ff
Jeremy Dawson <jeremy@cecs.anu.edu.au>
2021-07-08 18:47:24 +1000

    some distribution thms in ll_thms

--- ll/ll_thms.v
+++ ll/ll_thms.v
@@ -13,88 +13,87 @@ Require Import fmlsext.
 Require Import Coq.Program.Basics.
 Require Import lldefs ll_exch ll_camq.
 
-Lemma wth_sI {V} A B fs :
-  derrec maell_rules emptyT (A :: fs) ->
-  derrec maell_rules emptyT (B :: fs) ->
-  derrec maell_rules emptyT (@wth V A B :: fs).
+Lemma query_csI {V} lc A fs :
+  derrec maell_rules emptyT (lc ++ A :: fs) ->
+  derrec maell_rules emptyT (lc ++ @Query V A :: fs).
+Proof. intros.  eapply derI.  apply query_maellI.
+eapply (OSgctxt_eq _ _ _ lc _).  eapply Queryrule_I.
+reflexivity.  reflexivity.  apply (dersrec_singleI X). Qed.
+
+Definition query_sI {V} A fs := @query_csI V [] A fs.
+
+Lemma bang_csI {V} lc A fs :
+  derrec maell_rules emptyT (map (@Query V) lc ++ A :: map (@Query V) fs) ->
+  derrec maell_rules emptyT (map (@Query V) lc ++ Bang A :: map (@Query V) fs).
+Proof. intros.  eapply derI.  eapply bang_maellI.
+eapply (fmlsrulegq_I _ _ lc fs). eapply Bangrule_I.
+reflexivity.  reflexivity.  reflexivity.  reflexivity.
+apply (dersrec_singleI X). Qed.
+
+Lemma ctr_csI {V} lc A fs :
+  derrec maell_rules emptyT (lc ++ Query A :: Query A :: fs) ->
+  derrec maell_rules emptyT (lc ++ @Query V A :: fs).
+Proof. intros.  eapply derI.  eapply ctr_maellI.
+eapply (OSgctxt_eq _ _ _ lc _).  apply ctrrule_I.
+reflexivity.  reflexivity.  apply (dersrec_singleI X). Qed.
+
+Lemma wk_csI {V} lc A fs :
+  derrec maell_rules emptyT (lc ++ fs) ->
+  derrec maell_rules emptyT (lc ++ @Query V A :: fs).
+Proof. intros.  eapply derI.  eapply wk_maellI.
+eapply (OSgctxt_eq _ _ _ lc _).  apply wkrule_I.
+reflexivity.  reflexivity.  apply (dersrec_singleI X). Qed.
+
+Definition wk_sI {V} A fs := @wk_csI V [] A fs.
+
+Lemma wth_csI {V} lc A B fs :
+  derrec maell_rules emptyT (lc ++ A :: fs) ->
+  derrec maell_rules emptyT (lc ++ B :: fs) ->
+  derrec maell_rules emptyT (lc ++ @wth V A B :: fs).
 Proof. intros.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [] _).  apply Wth_p.  apply Wthrule_I.
+eapply (OSgctxt_eq _ _ _ lc _).  apply Wth_p.  apply Wthrule_I.
 reflexivity. reflexivity.  apply (dlCons X (dersrec_singleI X0)). Qed.
 
-Lemma wth_2sI {V} A B f fs :
-  derrec maell_rules emptyT (f :: A :: fs) ->
-  derrec maell_rules emptyT (f :: B :: fs) ->
-  derrec maell_rules emptyT (f :: @wth V A B :: fs).
-Proof. intros.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [_] _).  apply Wth_p.  apply Wthrule_I.
-reflexivity. reflexivity.  apply (dlCons X (dersrec_singleI X0)). Qed.
+Definition wth_sI {V} A B fs := @wth_csI V [] A B fs.
+Definition wth_2sI {V} A B f fs := @wth_csI V [f] A B fs.
 
-Lemma plusL_sI {V} A B fs :
-  derrec maell_rules emptyT (A :: fs) ->
-  derrec maell_rules emptyT (@plus V A B :: fs).
+Lemma plusL_csI {V} lc A B fs :
+  derrec maell_rules emptyT (lc ++ A :: fs) ->
+  derrec maell_rules emptyT (lc ++ @plus V A B :: fs).
 Proof. intro.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [] _).  apply PlusL_p.  apply PlusLrule_I.
+eapply (OSgctxt_eq _ _ _ lc _).  apply PlusL_p.  apply PlusLrule_I.
 reflexivity. reflexivity.  apply (dersrec_singleI X). Qed.
 
-Lemma plusR_sI {V} A B fs :
-  derrec maell_rules emptyT (B :: fs) ->
-  derrec maell_rules emptyT (@plus V A B :: fs).
+Lemma plusR_csI {V} lc A B fs :
+  derrec maell_rules emptyT (lc ++ B :: fs) ->
+  derrec maell_rules emptyT (lc ++ @plus V A B :: fs).
 Proof. intro.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [] _).  apply PlusR_p.  apply PlusRrule_I.
+eapply (OSgctxt_eq _ _ _ lc _).  apply PlusR_p.  apply PlusRrule_I.
 reflexivity. reflexivity.  apply (dersrec_singleI X). Qed.
 
-Lemma plusL_2sI {V} A B f fs :
-  derrec maell_rules emptyT (f :: A :: fs) ->
-  derrec maell_rules emptyT (f :: @plus V A B :: fs).
+Definition plusL_sI {V} A B fs := @plusL_csI V [] A B fs.
+Definition plusL_2sI {V} A B f fs := @plusL_csI V [f] A B fs.
+Definition plusR_sI {V} A B fs := @plusR_csI V [] A B fs.
+Definition plusR_2sI {V} A B f fs := @plusR_csI V [f] A B fs.
+
+Lemma par_csI {V} lc A B fs :
+  derrec maell_rules emptyT (lc ++ A :: B :: fs) ->
+  derrec maell_rules emptyT (lc ++ @par V A B :: fs).
 Proof. intro.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [_] _).  apply PlusL_p.  apply PlusLrule_I.
+eapply (OSgctxt_eq _ _ _ lc _).  apply Par_p.  apply Parrule_I.
 reflexivity. reflexivity.  apply (dersrec_singleI X). Qed.
 
-Lemma plusR_2sI {V} A B f fs :
-  derrec maell_rules emptyT (f :: B :: fs) ->
-  derrec maell_rules emptyT (f :: @plus V A B :: fs).
-Proof. intro.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [_] _).  apply PlusR_p.  apply PlusRrule_I.
-reflexivity. reflexivity.  apply (dersrec_singleI X). Qed.
-
-Lemma par_sI {V} A B fs :
-  derrec maell_rules emptyT (A :: B :: fs) ->
-  derrec maell_rules emptyT (@par V A B :: fs).
-Proof. intro.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [] _).  apply Par_p.  apply Parrule_I.
-reflexivity. reflexivity.  apply (dersrec_singleI X). Qed.
-
-Lemma par_2sI {V} A B f fs :
-  derrec maell_rules emptyT (f :: A :: B :: fs) ->
-  derrec maell_rules emptyT (f :: @par V A B :: fs).
-Proof. intro. eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [_] _).  apply Par_p.  apply Parrule_I.
-reflexivity. reflexivity.  apply (dersrec_singleI X). Qed.
-
-Lemma par_3sI {V} A B f1 f2 fs :
-  derrec maell_rules emptyT (f1 :: f2 :: A :: B :: fs) ->
-  derrec maell_rules emptyT (f1 :: f2 :: @par V A B :: fs).
-Proof. intro.  eapply derI.  apply princ_maellI.  
-eapply (OSgctxt_eq _ _ _ [_ ; _] _).  apply Par_p.  apply Parrule_I.
-reflexivity. reflexivity.  apply (dersrec_singleI X). Qed.
+Definition par_sI {V} A B fs := @par_csI V [] A B fs.
+Definition par_2sI {V} A B f fs := @par_csI V [f] A B fs.
+Definition par_3sI {V} A B f1 f2 fs := @par_csI V [f1 ; f2] A B fs.
 
 Lemma lolli_refl {V} A : derrec maell_rules emptyT [@lolli V A A].
 Proof. apply par_sI.  apply maell_idr. Qed.
 
 Lemma wpc_lem {V} A B :
   derrec maell_rules emptyT [plus (dual A) (dual B); @wth V B A].
-Proof. (* proof rather similar to that of maell_id *)
-eapply derI.  eapply princ_maellI.
-eapply (OSgctxt_eq _ _ _ [_] []). apply Wth_p.
-eapply Wthrule_I. reflexivity. reflexivity.
-simpl. unfold fmlsext. simpl.  apply dlCons. eapply derI.
-eapply princ_maellI. eapply (OSgctxt_eq _ _ _ [] [B]). apply PlusR_p.
-apply PlusRrule_I. reflexivity. reflexivity.
-simpl. unfold fmlsext. simpl. apply dersrec_singleI. apply maell_idr.
-apply dersrec_singleI. eapply derI.
-eapply princ_maellI. eapply (OSgctxt_eq _ _ _ [] [A]). apply PlusL_p.
-apply PlusLrule_I. reflexivity. reflexivity.
-simpl. unfold fmlsext. simpl. apply dersrec_singleI. apply maell_idr. Qed.
+Proof. apply wth_2sI. apply plusR_sI. apply maell_idr.
+apply plusL_sI. apply maell_idr. Qed.
 
 Lemma wth_comm {V} A B : 
   derrec maell_rules emptyT [@lolli V (wth A B) (wth B A)].
@@ -102,13 +101,9 @@ Proof. apply par_sI. apply wpc_lem. Qed.
 
 Lemma ptc_lem {V} A B :
   derrec maell_rules emptyT [tens (dual A) (dual B) ; @par V B A].
-Proof. (* proof rather similar to that of maell_id *)
-eapply derI.  eapply princ_maellI.
-eapply (OSgctxt_eq _ _ _ [tens _ _] []). apply Par_p.
-eapply Parrule_I. reflexivity. reflexivity.
-apply dersrec_singleI. unfold fmlsext. simpl.
-eapply derI. apply tens_maellI.
-eapply merge_ctxtgI. eapply eq_rect.
+(* proof rather similar to that of maell_id *)
+Proof. apply par_2sI.
+eapply derI. apply tens_maellI.  eapply merge_ctxtgI. eapply eq_rect.
 eapply (@merge_ctxtI _ _ _ _ [] [] [A] [B]). apply Tensrule_I.
 apply merge_nil. apply merge_simple_appr.  reflexivity. simpl.
 apply (dlCons (maell_idr _) (dlCons (maell_idr _) (dlNil _ _))). Qed.
@@ -172,12 +167,7 @@ apply dersrec_singleI. sfs. apply maell_idr. Qed.
 
 Lemma lolli_mp {V} A B : derrec maell_rules emptyT
   [@lolli V (tens (lolli A B) A) B].
-Proof. apply par_sI.  unfold lolli. simpl. rewrite dual_dual.
-eapply derI.  apply princ_maellI.
-eapply (OSgctxt_eq _ _ _ [] [_]).
-apply Par_p.  apply Parrule_I.
-reflexivity.  reflexivity.
-sfs.  apply dersrec_singleI.
+Proof. apply par_sI.  unfold lolli. simpl. rewrite dual_dual.  apply par_sI.
 eapply derI.  apply tens_maellI.  eapply merge_ctxtgI. eapply eq_rect.
 eapply (@merge_ctxtI _ _ _ _ [] [] [_] [_]). apply Tensrule_I.
 apply merge_nil.  apply merge_simple_app.  reflexivity. simpl.
@@ -332,14 +322,7 @@ Lemma query_cong {V} A B :
   derrec maell_rules emptyT [@lolli V A B] ->
   derrec maell_rules emptyT [lolli (Query A) (Query B)].
 Proof. intro lab. apply par_sI. simpl.
-eapply derI.  apply bang_maellI.
-eapply (fmlsrulegq_I _ _ [] [_]). eapply Bangrule_I.
-reflexivity.  reflexivity.  reflexivity.  reflexivity.
-apply dersrec_singleI. sfs.
-eapply derI.  apply query_maellI.
-eapply (OSgctxt_eq _ _ _ [_] []).  eapply Queryrule_I.
-reflexivity.  reflexivity.
-apply dersrec_singleI. sfs.  exact (par_inv lab). Qed.
+apply (bang_csI [] _ [_]). apply (query_csI [_]). exact (par_inv lab). Qed.
 
 Lemma bang_cong {V} A B : 
   derrec maell_rules emptyT [@lolli V A B] ->
@@ -350,7 +333,129 @@ apply query_cong in lab.
 apply (use_lolli (lolli_dual _ _)) in lab.
 simpl in lab. rewrite !dual_dual in lab. exact lab. Qed.
 
-Print Implicit bang_cong.
-
 (* from these could get congruence of eqv *)
+
+(* equivalences in the wikipedia page *)
+Lemma par_wth_fwd {V} A B C : derrec maell_rules emptyT
+  [@lolli V (par A (wth B C)) (wth (par A B) (par A C))].
+Proof. apply par_sI.  simpl. apply wth_2sI ; apply par_2sI ; apply tens_lemd.
+- apply plusL_sI. apply maell_idr.
+- apply plusR_sI. apply maell_idr. Qed.
+
+Lemma par_wth_bwd {V} A B C : derrec maell_rules emptyT
+  [@lolli V (wth (par A B) (par A C)) (par A (wth B C))].
+Proof. apply par_sI.  simpl. apply par_2sI.  apply (wth_csI [_ ; _]).
+- apply plusL_sI. apply tens_lemd. apply maell_idr.
+- apply plusR_sI. apply tens_lemd. apply maell_idr. Qed.
+
+Lemma tens_plus_fwd {V} A B C : derrec maell_rules emptyT
+  [@lolli V (tens A (plus B C)) (plus (tens A B) (tens A C))].
+Proof. pose (par_wth_bwd (dual A) (dual B) (dual C)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+Lemma tens_plus_bwd {V} A B C : derrec maell_rules emptyT
+  [@lolli V (plus (tens A B) (tens A C)) (tens A (plus B C))].
+Proof. pose (par_wth_fwd (dual A) (dual B) (dual C)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+Lemma bang_wth_bwd {V} A B : derrec maell_rules emptyT
+  [@lolli V (tens (Bang A) (Bang B)) (Bang (wth A B))].
+Proof. apply par_sI. simpl. apply par_sI. simpl.
+apply (bang_csI [_ ; _] _ []).  simpl.  apply (wth_csI [_ ; _]).
+- apply (wk_csI [_]).  apply query_sI.  apply maell_idr.
+- apply wk_sI.  apply query_sI.  apply maell_idr. Qed.
+
+Lemma bang_wth_fwd {V} A B : derrec maell_rules emptyT
+  [@lolli V (Bang (wth A B)) (tens (Bang A) (Bang B))].
+Proof. apply par_sI. simpl. apply (ctr_csI []).
+eapply derI.  apply tens_maellI.  eapply merge_ctxtgI. eapply eq_rect.
+eapply (@merge_ctxtI _ _ _ _ [_] [_] [] []). apply Tensrule_I.
+apply merge_simple_app.  apply merge_nil.  reflexivity. simpl.
+apply dlCons.  apply (bang_csI [_] _ []). apply query_sI.
+apply plusL_sI. apply maell_idr.
+apply dersrec_singleI.  apply (bang_csI [_] _ []). apply query_sI.
+apply plusR_sI. apply maell_idr. Qed.
+
+Lemma query_plus_fwd {V} A B : derrec maell_rules emptyT
+  [@lolli V (Query (plus A B)) (par (Query A) (Query B))].
+Proof. pose (bang_wth_bwd (dual A) (dual B)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+Lemma query_plus_bwd {V} A B : derrec maell_rules emptyT
+  [@lolli V (par (Query A) (Query B)) (Query (plus A B))].
+Proof. pose (bang_wth_fwd (dual A) (dual B)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+Print Implicit query_plus_bwd.
+
+(* linear distribution *)
+Lemma lin_dist {V} A B C : derrec maell_rules emptyT
+  [@lolli V (tens A (par B C)) (par (tens A B) C)].
+Proof. apply par_sI. simpl. apply par_2sI.  apply par_sI.  eapply exch_maell.
+2: apply (swapped_I [] [_] [_ ; _] [_] eq_refl eq_refl). 
+apply tens_lem.  eapply exch_maell.
+2: apply (swapped_I [] [_] [_] [_] eq_refl eq_refl). 
+apply tens_lemd. apply maell_idr. Qed.
+
+(* one-directional distributivity formulae *)
+Lemma bang_tens {V} A B : derrec maell_rules emptyT
+  [@lolli V (tens (Bang A) (Bang B)) (Bang (tens A B))].
+Proof. apply par_sI. simpl. apply par_sI.
+apply (bang_csI [_ ; _] _ []). simpl.
+eapply derI.  apply tens_maellI.  eapply merge_ctxtgI. eapply eq_rect.
+eapply (@merge_ctxtI _ _ _ _ [_] [_] [] []). apply Tensrule_I.
+apply merge_simple_app.  apply merge_nil.  reflexivity. simpl.
+apply dlCons. apply query_sI. apply maell_idr.
+apply dersrec_singleI. apply query_sI. apply maell_idr. Qed.
+
+Lemma bang_plus {V} A B : derrec maell_rules emptyT
+  [@lolli V (plus (Bang A) (Bang B)) (Bang (plus A B))].
+Proof. apply par_sI. simpl. 
+apply wth_sI ; apply (bang_csI [_] _ []) ; apply query_sI ; simpl.
+apply plusL_2sI. apply maell_idr.  apply plusR_2sI. apply maell_idr. Qed.
+
+Lemma query_par {V} A B : derrec maell_rules emptyT
+  [@lolli V (Query (par A B)) (par (Query A) (Query B))].
+Proof. pose (bang_tens (dual A) (dual B)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+Lemma query_wth {V} A B : derrec maell_rules emptyT
+  [@lolli V (Query (wth A B)) (wth (Query A) (Query B))].
+Proof. pose (bang_plus (dual A) (dual B)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+Lemma par_plus {V} A B C : derrec maell_rules emptyT
+  [@lolli V (plus (par A C) (par B C)) (par (plus A B) C)].
+Proof. apply par_sI. simpl. apply par_2sI. simpl.
+apply wth_sI. apply plusL_2sI. apply tens_lemd. apply maell_idr.
+apply plusR_2sI. apply tens_lemd. apply maell_idr. Qed.
+
+Lemma tens_wth {V} A B C : derrec maell_rules emptyT
+  [@lolli V (tens (wth A B) C) (wth (tens A C) (tens B C))].
+Proof. pose (par_plus (dual A) (dual B) (dual C)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+Lemma plus_wth {V} A B C : derrec maell_rules emptyT
+  [@lolli V (plus (wth A B) C) (wth (plus A C) (plus B C))].
+Proof. apply par_sI. simpl. apply wth_2sI ; apply wth_sI.
+- apply plusL_sI. apply plusL_2sI. apply maell_idr.
+- apply plusR_2sI. apply maell_idr.
+- apply plusR_sI. apply plusL_2sI. apply maell_idr.
+- apply plusR_2sI. apply maell_idr. Qed.
+
+Lemma wth_plus {V} A B C : derrec maell_rules emptyT
+  [@lolli V (plus (wth A C) (wth B C)) (wth (plus A B) C)].
+Proof. pose (plus_wth (dual A) (dual B) (dual C)).
+eapply (use_lolli (lolli_dual _ _)) in d. simpl in d.
+rewrite !dual_dual in d. exact d. Qed.
+
+
+
 
